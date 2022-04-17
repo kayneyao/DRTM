da2051c73dbbe83ca995bd500f3d17b65793f7cf
kayneyao <42885964+kayneyao@users.noreply.github.com>
2022-04-17 13:35:57 +0800

    speaking

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(){
 	cout<<"hello world!"<<endl;
 	cout<<"THE MAIN"<<endl;
 	
-	cout<<"MB:"<<""<<endl;
+	cout<<"MB:"<<"fowenriuvgwdconwoerbidcmixcbiqer"<<endl;
 	cout<<"KK:"<<""<<endl;
 	cout<<"WT:"<<""<<endl;
 	
